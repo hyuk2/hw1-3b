c81162b1477531481971b3a53826ca037187453f
hyuk92 <hyuk9292@naver.com>
2017-04-18 23:48:58 +0900

    Commit 3

--- Hi/src/Hi.java
+++ Hi/src/Hi.java
@@ -3,5 +3,6 @@ public class Hi {
     public static void main(String[] ar){
     System.out.println("첫번째 커밋하기 전!");
     System.out.println("첫번째 커밋 완료 , 두번째 커밋하기 전입니다.");
+    System.out.println("두번째 커밋 완료 , 세번째 커밋하기 전입니다.");
     }
 }
